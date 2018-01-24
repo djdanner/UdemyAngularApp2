b2571f3036aa40694955e6c2de21ecea09ec24f9
djdanner <DJDanner@Gmail.com>
2018-01-24 05:24:35 -0500

    End 48.

--- app/recipes/recipe-list/recipe-list.component.ts
+++ app/recipes/recipe-list/recipe-list.component.ts
@@ -1,13 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
-  
+  recipes: Recipe[] = [
+    new Recipe(
+      'Test Recipe Name',
+      'Test Recipe Description',
+      'https://cdn.pixabay.com/photo/2017/07/04/17/05/mango-2471837_960_720.jpg'
+    )
+  ];
+
   constructor() { }
 
   ngOnInit() {
