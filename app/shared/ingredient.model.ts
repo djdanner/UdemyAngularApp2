365511ed255b075c3934efda3393e32c1a4115eb
djdanner <DJDanner@Gmail.com>
2018-01-24 23:36:25 -0500

    Added Ingredients Framework.

---
+++ app/shared/ingredient.model.ts
@@ -0,0 +1,6 @@
+export class Ingredient{
+  // This is a common, short-hand TypeScript syntax for defining these
+  // Class Properties and then initializing them via the Constructor.
+  // Basically the Property Declaration and Initialization code are combined.
+  constructor(public name: String, public amount: Number){}
+}
